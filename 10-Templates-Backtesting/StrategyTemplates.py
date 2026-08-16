aa89a1571acc64ec705a503161476b8dc108e80a
Pablo-Fernandez-001 <2968159181905@ingenieria.usac.edu.gt>
2026-08-16 13:31:13 -0600

    features: 10th part done

---
+++ 10-Templates-Backtesting/StrategyTemplates.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 23:32:47 2026
+
+@author: pabda
+"""
+
+# Standard Libraries
+...
+# Custom Libraries
+...
+
+# Strategy Class
+class StrategyN:
+    
+    """ 
+    Strategy {}: ...
+    
+        Description:
+            
+            ...
+    
+    Strategy For:
+        
+        -
+        -
+        -
+        -
+        
+    Frequencies (Time Frames):
+        
+        - 
+        -
+        -
+        -
+        
+    Holding Period:
+        
+        - 
+        -
+        -
+        
+    
+    Analysis Used: 
+        
+        -
+        -
+        -
+    
+    Detailed Strategy Description:    
+        
+        ...
+                        
+        Stop Loss and Take Profit Description:
+            
+            Take Profit:
+                
+                ...
+                
+            Stop Loss:
+                
+                ...
+            
+    Co-Integration (If Multiple Indicators Are Used):
+        
+        Signals:
+            
+            ...
+            
+        Trade Exit:
+            
+            ...
+            
+    General Assumptions:
+        
+        ...
+        
+    Notes:
+        
+        ...
+    """
+    
+    __version__ = 1.0
+    
+    # __init__
+    def __init__(self) -> None:
+        
+        """
+        Constructor.
+        
+        Parameters
+        ----------
+
+                
+        
+        Output
+        -------
+        return: 
+        """
+        
+        # Attributes
+         
+        # Private Attributes
+        
+    
+    # __repr__
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + ".class"
+    
+    
+    # Backtest
+    def backtest(self):
+        
+        """
+        ...
+        
+        Parameters
+        ----------
+
+                
+        
+        Output
+        -------
+        return: 
+        """
+        
+        # Calculate
+        
+        return 
+    
+    
+    # Calculate
+    def calculate(self):
+        
+        """
+        ...
+        
+        Parameters
+        ----------
+
+                
+        
+        Output
+        -------
+        return: 
+        """
+        
+        # Calculate   
+
+        return
+    
+    
+    # Optimize
+    def optimize(self):
+        
+        """
+        ...
+        
+        Parameters
+        ----------
+
+                
+        
+        Output
+        -------
+        return: 
+        """
+        
+        # Optimize
+        
+        return
+    
+    
+    # Plot
+    def plot(self):
+        
+        """
+        ...
+        
+        Parameters
+        ----------
+
+                
+        
+        Output
+        -------
+        return: 
+        """
+        
+        # Calculate
+        
+        return
